4b53ddfce5d99eba716acd0388f0dec524437097
marilynamendiares <innkonovalov@gmail.com>
2026-02-10 20:57:49 +0200

    troubleshooting

--- src/server/r2.ts
+++ src/server/r2.ts
@@ -1,6 +1,8 @@
 // src/server/r2.ts
 import { S3Client } from "@aws-sdk/client-s3";
 
+export const R2_BUCKET = process.env.R2_BUCKET!;
+
 export const r2 = new S3Client({
   region: "auto",
   endpoint: process.env.R2_ENDPOINT!,
